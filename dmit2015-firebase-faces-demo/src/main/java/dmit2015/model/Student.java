75a28228cd01cc508ce6a3540608e071015e30e8
Sam Wu <swu@nait.ca>
2024-09-16 20:20:23 -0600

    Added Student dataset

---
+++ dmit2015-firebase-faces-demo/src/main/java/dmit2015/model/Student.java
@@ -0,0 +1,15 @@
+package dmit2015.model;
+
+import lombok.Data;
+
+/**
+ * The structure of data stored FirebaseRTDB
+ */
+@Data
+public class Student {
+
+    private String name;    // Firebase Unique Identifier
+    private String firstName;   // Student first name
+    private String lastName;    // Student last name
+    private String email;       // Student email
+}
